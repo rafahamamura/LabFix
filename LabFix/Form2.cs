1f6fc0aa210a682f5f20ca216b59d3ad175843d4
Rafael Hamamura <rafael.hamamura@fatec.sp.gov.br>
2024-06-25 14:15:19 -0300

    Add setup IP

--- LabFix/Form2.cs
+++ LabFix/Form2.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
+using System.Net;
 
 namespace LabFix
 {
@@ -24,6 +28,8 @@ namespace LabFix
         NetworkInterface selectedNIC;
         String network = "10.10";
         String ip = ".1.10";
+        String ipFull = "";
+        String gateway = "10.10.0.1";
         public frm_configIP()
         {
             InitializeComponent();
@@ -31,10 +37,12 @@ namespace LabFix
 
         private void frm_configIP_Load(object sender, EventArgs e)
         {
-            comboBox1.Items.AddRange(labList);
-            comboBox1.SelectedIndex = 0;
-            comboBox2.Items.AddRange(machineList);
-            comboBox2.SelectedIndex = 0;
+            cbLabs.Items.AddRange(labList);
+            cbLabs.SelectedIndex = 0;
+            cbHosts.Items.AddRange(machineList);
+            cbHosts.SelectedIndex = 0;
+
+            checkHostname();
 
             // Load interfaces description into comboBox
             int indexCB = 0; // selected item in comboBox
@@ -47,7 +55,7 @@ namespace LabFix
                     networkInterface.NetworkInterfaceType.Equals(NetworkInterfaceType.Ethernet))
                 {
                     string desc = networkInterface.Description;
-                    comboBox3.Items.Add(desc);
+                    cbInterfaces.Items.Add(desc);
                     if (desc.StartsWith("Realtek") ||
                         desc.StartsWith("Intel") ||
                         desc.StartsWith("Broadcom") ||
@@ -60,37 +68,83 @@ namespace LabFix
                 }
 
             }
-            comboBox3.SelectedIndex = indexCB;
+            cbInterfaces.SelectedIndex = indexCB;
 
+        }
+
+        private void checkHostname()
+        {
+            IPGlobalProperties prop = IPGlobalProperties.GetIPGlobalProperties();
+            if (prop.HostName.Contains("LAB"))
+            {
+                string[] hostname = prop.HostName.Split('-');
+                cbLabs.SelectedText = hostname[0];
+                cbHosts.SelectedText = hostname[1];
+            }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            setIP();
+            setIpFull();
         }
 
-        private void setIP()
+        private void setIpFull()
         {
-            int ipLab = (int)comboBox1.SelectedIndex + 1;
-            int ipMac = (int)comboBox2.SelectedIndex + 10;
+            int ipLab = (int)cbLabs.SelectedIndex + 1;
+            int ipMac = (int)cbHosts.SelectedIndex + 10;
 
             ip = "." + ipLab + "." + ipMac;
+            ipFull = network + ip;
 
-            txtBoxIP.Text = network + ip;
+            txtBoxIP.Text = ipFull;
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            setIP();
+            setIpFull();
         }
 
         private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
             foreach (NetworkInterface networkInterface in networkInterfaces)
             {
-                if (networkInterface.Description.Equals(comboBox3.SelectedText))
+                if (networkInterface.Description.Equals(cbInterfaces.SelectedText))
                 {
                     selectedNIC = networkInterface;
+                }
+            }
+        }
+
+        private void setConfig()
+        {
+            var adapterConfig = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            var networkCollection = adapterConfig.GetInstances();
+            foreach (ManagementObject adapter in networkCollection)
+            {
+                string description = adapter["Description"] as string;
+                if (String.Compare(description, selectedNIC.Description) == 0)
+                {
+                    try
+                    {
+                        // Set DefaultGateway
+                        var newGateway = adapter.GetMethodParameters("SetGateways");
+                        newGateway["DefaultIPGateway"] = new string[] { gateway };
+                        newGateway["GatewayCostMetric"] = new int[] { 1 };
+
+                        // Set IPAddress and Subnet Mask
+                        var newAddress = adapter.GetMethodParameters("EnableStatic");
+                        newAddress["IPAddress"] = new string[] { ipFull };
+                        newAddress["SubnetMask"] = new string[] { "255.255.255.0" };
+
+                        adapter.InvokeMethod("EnableStatic", newAddress, null);
+                        adapter.InvokeMethod("SetGateways", newGateway, null);
+
+                        txtLog.Text.Insert(0,"Updated to static IP address!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(this, "Unable to Set IP : " + ex.Message);
+                    }
                 }
             }
         }
